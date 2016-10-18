9e40ac84b4e5b7903375d2f6abd446911018cf38
Allan <chenyongjian>
2016-10-18 09:32:06 +0800

    add files

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PhotoGrid = React.createClass({
+  render(){
+    return(
+      <p>I am PhotoGrid</p>
+    );
+  }
+})
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render(){
+    return(
+      <p>I am Single</p>
+    );
+  }
+})
+
+export default Single;
